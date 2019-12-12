2f2ee46b25863ebd8d8bff95bb919780c6aad1a0
Ronmor <Ronmor35@gmail.com>
2019-12-12 19:21:17 +0200

    firebase registeration is better dependencies updated

--- app/src/main/java/com/example/playmix/MainActivity.java
+++ app/src/main/java/com/example/playmix/MainActivity.java
@@ -24,15 +24,18 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
     FirebaseAuth auth;
-    FirebaseUser user;
+    FirebaseUser firebaseUser;
     boolean test = false;
     LoginButton loginButton;
     CallbackManager callbackManager;
@@ -41,11 +44,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+       // databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(auth.getInstance().getCurrentUser().getUid());
+        firebaseUser = auth.getCurrentUser();
         setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-        if(user == null)
+        if(firebaseUser == null)
         {
             //loginButton = (LoginButton)findViewById(R.id.login_button);
             setUI();
@@ -63,9 +68,28 @@ public class MainActivity extends AppCompatActivity {
     private void setUI() {
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
-        //loginButton.setOnClickListener();
         loginButton.setPermissions(Arrays.asList("email"));
+        //loginButton.setOnClickListener((View.OnClickListener) this);
 
+    }
+
+    public void onClick(View v){
+    if (v==loginButton){
+            User user = new User(auth.getCurrentUser().getEmail(),auth.getCurrentUser().getDisplayName());
+            databaseReference = firebaseDatabase.getReference();
+            DatabaseReference.CompletionListener completionListener = new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                    if (databaseError!=null){
+                        Toast.makeText(MainActivity.this,databaseError.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this,"Good",Toast.LENGTH_LONG).show();
+                    }
+                }
+            };
+        databaseReference.child("users").child(auth.getCurrentUser().getEmail()).setValue(user,completionListener);
+    }
     }
 
     @Override
@@ -81,12 +105,13 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 HAndleFacebookToken(loginResult.getAccessToken());
                 Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_LONG).show();
-                startActivity(new Intent(MainActivity.this,FreeUser.class));
+                addUser();
+                startActivity(new Intent(MainActivity.this, UserProfile.class));
             }
 
             @Override
             public void onCancel() {
-                Toast.makeText(getApplicationContext(), "user cancelled it", Toast.LENGTH_LONG).show();;
+                Toast.makeText(getApplicationContext(), "firebaseUser cancelled it", Toast.LENGTH_LONG).show();;
 
             }
 
@@ -96,7 +121,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void addUser(){
+        databaseReference=firebaseDatabase.getReference();
+        Toast.makeText(this,"Addednreognwesofnwoelgnfweofmwfmew",Toast.LENGTH_LONG).show();
+        if (auth.getCurrentUser().getEmail()!=null){
+            String id = databaseReference.push().getKey();
+            User user = new User(auth.getCurrentUser().getEmail(),auth.getCurrentUser().getDisplayName());
+            user.setId(id);
+            databaseReference.child(id).setValue(user);
+            Toast.makeText(this,"Added",Toast.LENGTH_LONG).show();
+        }
 
+    }
     private void HAndleFacebookToken(AccessToken accessToken)
     {
         AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
@@ -119,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void validate(View view){
         if (test){
-            Intent userScreen = new Intent(MainActivity.this , FreeUser.class);
+            Intent userScreen = new Intent(MainActivity.this , UserProfile.class);
         }
         else {
             Toast.makeText(getApplicationContext(),"blabla",Toast.LENGTH_LONG).show();
