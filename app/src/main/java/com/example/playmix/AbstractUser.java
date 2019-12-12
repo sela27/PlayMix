1c7a8c5f4ab9ed55d847827d2f1a936958570a98
Ronmor <Ronmor35@gmail.com>
2019-12-12 19:21:58 +0200

    a draw of the user class

---
+++ app/src/main/java/com/example/playmix/AbstractUser.java
@@ -0,0 +1,45 @@
+package com.example.playmix;
+
+public class AbstractUser {
+
+    protected static final long FREE_USER=0;
+    protected static final long GOLD_USER=1;
+    protected static final long PLATINUM_USER=2;
+    private String email;
+    private String name;
+    private long status;
+    private String id;
+
+    public AbstractUser() {
+    }
+
+    public AbstractUser(String email, String name) {
+        this.email = email;
+        this.name = name;
+        this.status = FREE_USER;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getStatus() {
+        return status;
+    }
+
+    public void setStatus(long status) {
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/playmix/User.java
@@ -0,0 +1,11 @@
+package com.example.playmix;
+
+public class User extends AbstractUser {
+
+    public User(String email, String name) {
+        super(email, name);
+    }
+
+    public User() {
+    }
+}
